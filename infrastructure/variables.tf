da79657665d3aaa1f4d0183ecfff2b7e620f2836
Gregory Dearing <71654340+gregorydearing@users.noreply.github.com>
2025-09-23 11:23:20 +0200

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,4 +1,6 @@
+# -------------------
 # AWS Settings
+# -------------------
 variable "aws_region" {
   description = "AWS region to deploy resources in"
   type        = string
@@ -11,7 +13,9 @@ variable "availability_zone" {
   default     = ""
 }
 
+# -------------------
 # Networking
+# -------------------
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
@@ -24,7 +28,9 @@ variable "public_subnet_cidr" {
   default     = "10.0.1.0/24"
 }
 
+# -------------------
 # EC2
+# -------------------
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
@@ -35,5 +41,38 @@ variable "ami_id" {
   description = "AMI ID for EC2 instance"
   type        = string
   default     = "ami-09278528675a8d54e"
+}
+
+# -------------------
+# RDS (PostgreSQL)
+# -------------------
+variable "db_name" {
+  description = "Name of the PostgreSQL database"
+  type        = string
+  default     = "grocerymate-db"
+}
+
+variable "db_username" {
+  description = "Master username for PostgreSQL"
+  type        = string
+  default     = "grocery_user"
+}
+
+variable "db_password" {
+  description = "Master password for PostgreSQL"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_instance_class" {
+  description = "RDS instance type"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "db_allocated_storage" {
+  description = "Storage allocated to RDS instance (in GB)"
+  type        = number
+  default     = 20
 }
 
